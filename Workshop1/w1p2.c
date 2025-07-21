772f84dee45c2be4ac3b9bdf971ab6447f126f12
Ulaş Şengüler <93615125+ulassenguler@users.noreply.github.com>
2025-07-21 13:33:51 +0300

    Add files via upload

---
+++ Workshop1/w1p2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+printf("Workshop 1 Part-2\n");
+printf("=================\n\n");
+printf("Using tab specifiers...\n");
+printf("\tTab-1\tTab-2\tTab-3\t\n");
+printf("\t-----\t=====\t-----\n");
+printf("\\ this is a back-slash character!\n\n");
+printf("%% this is a percent sign character!\n\n"); 
+printf("\"  this is a double-quote character!\n\n");
+ 
+printf("1.\t\"Skill is only developed by hours and hours of work.\"\n"); 
+printf("\t\t\t\t\t\t -Usain Bolt\n");
+
+printf("2.\t\"It's not about having time. It's about making time.\"\n");
+printf("\t\t\t\t\t\t -unknown\n");
+
+printf("3.\t\"All of us do not have equal talent. But, all of us");
+printf("\n\thave an equal opportunity to develop our talents.\n ");
+printf("\t\t\t\t\t\t -A.P.J. Abdul Kalam\n");
+
+    
+    getchar();
+
+    
+return 0;
+
+}
